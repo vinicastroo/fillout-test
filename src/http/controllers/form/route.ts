8a9a08898ce68695c1913ea609c6d8d9a2aad1f8
Vinicius Castro <viniciusc.d.c@hotmail.com>
2024-03-05 17:26:37 -0300

    feat/change routes

--- src/http/controllers/form/route.ts
+++ src/http/controllers/form/route.ts
@@ -2,5 +2,8 @@ import { FastifyInstance } from 'fastify'
 import { search } from './search'
 
 export async function formRoutes(app: FastifyInstance) {
-  app.get('/', search)
+  app.get('/', (_, reply) => {
+    return reply.status(200).send({ ok: 'ok' })
+  })
+  app.get('/form', search)
 }
